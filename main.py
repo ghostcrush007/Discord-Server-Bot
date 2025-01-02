54261867c4f7108789a7941bd76c59386ba25897
Bibek Kumar Jena <ghostcrush007@gmail.com>
2025-01-02 17:24:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class MyClient(discord.Client):
 
                 client = Groq()
                 completion = client.chat.completions.create(
-                model="llama-3.1-70b-versatile",
+                model="llama-3.3-70b-versatile",
                 messages= [{"role": "system", "content": "You are a helpful AI bot."},
                            {"role": "user", "content": user_input}],
                 temperature=0.3,
